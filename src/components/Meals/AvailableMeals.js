97712dc76cff427fec661ba4a5d053730c81467d
eid <eidhachem1@gmail.com>
2024-09-10 20:40:58 +0300

    Add meals list and styles

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,6 +2,9 @@ import React from "react"
 
 import classes from "./AvailableMeals.module.css"
 
+import Card from "../UI/Card"
+import MealItem from "./MealItem/MealItem"
+
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -31,11 +34,18 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <li key={meal.id}>{meal.name}</li>
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
   ))
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   )
 }
