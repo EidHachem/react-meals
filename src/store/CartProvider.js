233e97b0fa00e0d654f74f2b2db07542076f94d5
eid <eidhachem1@gmail.com>
2024-09-11 00:52:43 +0300

    Add context to control cart actions

---
+++ src/store/CartProvider.js
@@ -0,0 +1,46 @@
+import { useReducer } from "react"
+import CartContext from "./cart-context"
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+}
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item)
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    }
+  }
+  return defaultCartState
+}
+
+const CartProvider = ({ children }) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  )
+  const addItemToCart = (item) => {
+    dispatchCartAction({ type: "ADD", item })
+  }
+
+  const removeItemFromCart = (id) => {
+    dispatchCartAction({ type: "REMOVE", id })
+  }
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCart,
+    removeItem: removeItemFromCart,
+  }
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  )
+}
+
+export default CartProvider
